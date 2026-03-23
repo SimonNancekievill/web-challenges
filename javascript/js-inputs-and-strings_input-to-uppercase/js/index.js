481b79819aabfe99535feabac03978708d7c26e8
SimonNancekievill <simon.nancekievill@gmail.com>
2026-03-23 14:53:37 +0100

    solved input to uppercase challenge

--- javascript/js-inputs-and-strings_input-to-uppercase/js/index.js
+++ javascript/js-inputs-and-strings_input-to-uppercase/js/index.js
@@ -1,1 +1,9 @@
 console.clear();
+const firstInput = document.querySelector('[data-js="first-input"]');
+const button = document.querySelector('[data-js="button-uppercase"]');
+
+button.addEventListener("click", () => {
+  const lowercaseInput = firstInput.value;
+  const uppercaseInput = lowercaseInput.toUpperCase();
+  firstInput.value = uppercaseInput;
+});
