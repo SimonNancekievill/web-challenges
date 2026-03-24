55db9d3d7a0ae7aa8c7611d2f88a4d7dad7e3bfd
SimonNancekievill <simon.nancekievill@gmail.com>
2026-03-24 12:21:59 +0100

    solving checkbox input challenge

--- javascript/java-script-forms/js-forms_checkbox-input/js/index.js
+++ javascript/java-script-forms/js-forms_checkbox-input/js/index.js
@@ -3,6 +3,7 @@ console.clear();
 const form = document.querySelector('[data-js="form"]');
 const tosError = document.querySelector('[data-js="tos-error"]');
 const tosCheckbox = document.querySelector('[data-js="tos"]');
+const formSuccess = document.querySelector('[data-js="success"]');
 
 function hideTosError() {
   tosError.setAttribute("hidden", "");
@@ -12,13 +13,31 @@ function showTosError() {
   tosError.removeAttribute("hidden");
 }
 
+function showFormSuccess() {
+  formSuccess.removeAttribute("hidden");
+}
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   // --v-- write your code here --v--
+  if (tosCheckbox.checked === true) {
+    alert("Form submitted");
+    hideTosError();
+    showFormSuccess();
+  } else {
+    alert("To continue, please accept our Terms of Service!");
+    showTosError();
+  }
 
   // --^-- write your code here --^--
 
   // eslint-disable-next-line no-alert
-  alert("Form submitted");
+  // alert("Form submitted");
+});
+tosCheckbox.addEventListener("input", () => {
+  if (tosCheckbox.checked === true) {
+    hideTosError();
+  } else {
+    showTosError();
+  }
 });
