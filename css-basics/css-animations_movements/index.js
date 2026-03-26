9be170f35ca8cdbb86f2fc227c9fcd324c2ee321
SimonNancekievill <simon.nancekievill@gmail.com>
2026-03-26 13:41:45 +0100

    solving 2/3 of the challenge

--- css-basics/css-animations_movements/index.js
+++ css-basics/css-animations_movements/index.js
@@ -4,11 +4,17 @@
 
 const changeColorButton = document.querySelector("[data-js=changeColorButton]");
 const colorChangeCard = document.querySelector("[data-js=colorChangeCard]");
-const colors = ["#e0f7fa", "#ffe0b2", "#c8e6c9", "#ffccbc", "#d1c4e9"]; // Light pastel colors
+const colors = ["#e0e7fa", "#ffe0b2", "#c8e6c9", "#ffccbc", "#d1c4e9"]; // Light pastel colors
 let currentColorIndex = 0;
 
 changeColorButton.addEventListener("click", () => {
-  // Add your solution below
+  currentColorIndex++;
+  if (currentColorIndex > 4) {
+    currentColorIndex = 0;
+  }
+  colorChangeCard.style.backgroundColor = colors[currentColorIndex];
+
+  console.log(currentColorIndex);
 });
 
 /*  -----------------------------------------------------------
@@ -22,15 +28,31 @@ changeColorButton.addEventListener("click", () => {
             Challenge 3: Loading Spinner Toggle
 ---------------------------------------------------------------- */
 const toggleLoaderButton = document.querySelector(
-  "[data-js=toggleLoaderButton]"
+  "[data-js=toggleLoaderButton]",
 );
 const loaderContainer = document.querySelector("[data-js=loaderContainer]");
 let loaderActive = false;
 
 toggleLoaderButton.addEventListener("click", () => {
-  // Add your solution below
+  if (loaderActive === false) {
+    loaderContainer.classList.add("active");
+    loaderActive = true;
+  } else {
+    loaderContainer.classList.remove("active");
+    loaderActive = false;
+  }
 });
+// toggleLoaderButton.addEventListener("click", () => {
+//   // Add your solution below
 
+//   if (loaderActive === false) {
+//     loaderContainer.classList.add("active");
+//     loaderActive = true;
+//   } else {
+//     loaderContainer.classList.remove("active");
+//     loaderActive = false;
+//   }
+// });
 /*  -----------------------------------------------------------
             Challenge 4: Slide-in Div
 ---------------------------------------------------------------- */
