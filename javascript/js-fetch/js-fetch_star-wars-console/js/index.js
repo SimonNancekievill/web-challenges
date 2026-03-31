0997a2a99feae152b481818b36f96c920075bed9
Simon Nancekievill <simon.nancekievill@gmail.com>
2026-03-31 15:52:03 +0200

    solving star wars console challenge

--- javascript/js-fetch/js-fetch_star-wars-console/js/index.js
+++ javascript/js-fetch/js-fetch_star-wars-console/js/index.js
@@ -1,7 +1,16 @@
-console.clear();
+// console.clear();
 
 const url = "https://swapi.py4e.com/api/people";
 
-function fetchData() {}
-
+async function fetchData() {
+  const response = await fetch(url);
+  const data = await response.json();
+  const results = data.results;
+  //   results.forEach((result) => {
+  //     console.log("Name: " + result.name);
+  //   });
+  console.log(results);
+  console.log(results[2].eye_color);
+  return data;
+}
 fetchData();
