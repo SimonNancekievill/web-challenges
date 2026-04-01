2e402a3c0fbef715d5b27424d429362e736a8dc1
Simon Nancekievill <simon.nancekievill@gmail.com>
2026-04-01 15:00:43 +0200

    solving box appearance challenge

--- javascript/js-inputs-and-strings/js-inputs-and-strings_change-box-appearance/js/index.js
+++ javascript/js-inputs-and-strings/js-inputs-and-strings_change-box-appearance/js/index.js
@@ -1,1 +1,20 @@
 console.clear();
+const boxElement = document.querySelector('[data-js="box"]');
+const colourInput = document.querySelector('[data-js="input-color"]');
+const radiusInput = document.querySelector('[data-js="input-radius"]');
+const rotationInput = document.querySelector('[data-js="input-rotation"]');
+
+// EventListener for colourInput //
+colourInput.addEventListener("input", () => {
+  boxElement.style.background = `hsl(${colourInput.value}, 70%, 60%)`;
+});
+
+// EventListener for radiusInput //
+radiusInput.addEventListener("input", () => {
+  boxElement.style.borderRadius = `${radiusInput.value}%`;
+});
+
+console.log(rotationInput);
+rotationInput.addEventListener("input", () => {
+  boxElement.style.transform = `rotate(${rotationInput.value}deg)`;
+});
