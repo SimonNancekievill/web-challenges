79b2d208e90512e3dbc37ad7fa16e2c6c1d76948
Simon Nancekievill <simon.nancekievill@gmail.com>
2026-04-10 15:33:08 +0200

    solving article challenge

--- react/react-basics/react-basics_hello-world-article/src/App.jsx
+++ react/react-basics/react-basics_hello-world-article/src/App.jsx
@@ -1,5 +1,20 @@
 import "./styles.css";
 
 export default function App() {
-  return <div>Replace me with your Component!</div>;
+  return HelloWorldArticle();
+}
+
+function HelloWorldArticle() {
+  return (
+    <article>
+      <h1>Hello World!</h1>
+      <p>
+        This is my first React Component which uses a wrapper to return more
+        than one element.
+      </p>
+      <button type="button" onClick={() => console.log("I am a Dolphin!")}>
+        🐬
+      </button>
+    </article>
+  );
 }
