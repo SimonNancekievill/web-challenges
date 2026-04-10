ddfbd88b430b8dff9290c354ddb502d35b04478e
Simon Nancekievill <simon.nancekievill@gmail.com>
2026-04-10 16:14:11 +0200

    solving attributes challengeä

--- react/react-basics/react-basics_attributes/src/App.jsx
+++ react/react-basics/react-basics_attributes/src/App.jsx
@@ -1,5 +1,23 @@
 import "./styles.css";
 
 export default function App() {
-  return <article>Replace me with an article</article>;
+  return Article();
+}
+
+function Article() {
+  return (
+    <article className="article">
+      <h2 className="article__title">I am a H2 Element!</h2>
+      <label htmlFor="articleInput">
+        <input id="articleInput"></input>
+      </label>
+      <a
+        className="article__link"
+        href="https://de.wikipedia.org/wiki/React"
+        target="blank"
+      >
+        Find out more about React!
+      </a>
+    </article>
+  );
 }
