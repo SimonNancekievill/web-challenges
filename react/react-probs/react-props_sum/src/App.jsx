f59206716ee8024d690cc5c21567ad43bf7fa4c6
Simon Nancekievill <simon.nancekievill@gmail.com>
2026-04-13 11:22:00 +0200

    solving sum challenge

--- react/react-probs/react-props_sum/src/App.jsx
+++ react/react-probs/react-props_sum/src/App.jsx
@@ -1,3 +1,15 @@
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return (
+    <div>
+      <Sum valueA={20} valueB={67} />
+    </div>
+  );
+}
+
+function Sum({ valueA, valueB }) {
+  return (
+    <div>
+      {valueA} + {valueB} = {valueA + valueB}
+    </div>
+  );
 }
