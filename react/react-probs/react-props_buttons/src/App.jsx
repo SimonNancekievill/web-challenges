9b9bbe59ad07c0ac35e826b9d37bce340e991b24
Simon Nancekievill <simon.nancekievill@gmail.com>
2026-04-13 15:17:27 +0200

    solving probs challenge

--- react/react-probs/react-props_buttons/src/App.jsx
+++ react/react-probs/react-props_buttons/src/App.jsx
@@ -1,3 +1,25 @@
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  function handleClick(text) {
+    console.log(`You clicked the Button: ${text}`);
+  }
+  return (
+    <div>
+      <Button text="Submit" color="#1fff53" onClick={handleClick} />
+      <Button text="Danger" color="rgb(255, 7, 7)" onClick={handleClick} />
+      <Button text="Disabled" disabled onClick={handleClick} />
+    </div>
+  );
+}
+
+function Button({ color, disabled, text, onClick }) {
+  return (
+    <button
+      type="button"
+      disabled={disabled}
+      style={{ backgroundColor: disabled ? "grey" : color, height: "100px" }}
+      onClick={() => onClick(text)}
+    >
+      {text}
+    </button>
+  );
 }
