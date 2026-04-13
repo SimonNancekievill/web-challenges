fce09ac817d9c6fe52d8a5b082c7ae4a663d0de0
Simon Nancekievill <simon.nancekievill@gmail.com>
2026-04-13 11:40:33 +0200

    soling smiley challenge

--- react/react-probs/react-props_smiley/src/App.jsx
+++ react/react-probs/react-props_smiley/src/App.jsx
@@ -1,3 +1,11 @@
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  return (
+    <div>
+      <Smiley isHappy />
+    </div>
+  );
+}
+
+function Smiley({ isHappy }) {
+  return <div>{isHappy ? <span>😁</span> : <span>😒</span>}</div>;
 }
