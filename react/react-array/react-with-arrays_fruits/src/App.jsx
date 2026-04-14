1915ca00cf6e9483cd73b9b5f41fe5f1137194c4
Simon Nancekievill <simon.nancekievill@gmail.com>
2026-04-14 15:16:42 +0200

    solving fruits challenge

--- react/react-array/react-with-arrays_fruits/src/App.jsx
+++ react/react-array/react-with-arrays_fruits/src/App.jsx
@@ -1,11 +1,40 @@
 import Card from "./components/Card";
 
 export default function App() {
-  const fruits = [];
-
+  const fruits = [
+    {
+      id: 1337,
+      name: "🍌 Banana",
+      color: "yellow",
+    },
+    {
+      id: 1234,
+      name: "🥝 Kiwi",
+      color: "green",
+    },
+    {
+      id: 4567,
+      name: "🍓 Strawberry",
+      color: "red",
+    },
+    {
+      id: 9876,
+      name: "🍏 Apple",
+      color: "green",
+    },
+    {
+      id: 5732,
+      name: "🍉 Watermelon",
+      color: "green, red",
+    },
+  ];
   return (
     <div className="app">
-      <Card name="🍌 banana" />
+      {fruits.map((fruit) => (
+        <li key={fruit.id}>
+          <Card name={fruit.name} color={fruit.color} id={fruit.id} />
+        </li>
+      ))}
     </div>
   );
 }
