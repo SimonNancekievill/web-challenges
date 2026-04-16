e9e7c3627613678e6bb6032de217675740745450
Simon Nancekievill <simon.nancekievill@gmail.com>
2026-04-16 11:15:22 +0200

    solving colored number challenge

--- react/react-state-2/react-state-2_colored-number/src/components/Counter.jsx
+++ react/react-state-2/react-state-2_colored-number/src/components/Counter.jsx
@@ -1,15 +1,27 @@
+import { useState } from "react";
 import ColoredNumber from "./ColoredNumber";
 
 export default function Counter() {
+  const [count, setCounter] = useState(0);
+
+  function handleIncreaseCounter() {
+    setCounter(count + 1);
+    console.log(count);
+  }
+  function handleDecreaseCounter() {
+    setCounter(count - 1);
+    console.log(count);
+  }
   return (
     <div className="counter">
       <h1>Colored Number</h1>
-      <ColoredNumber value={0} />
+      <ColoredNumber value={count} />
       <div className="counter__buttons-container">
         <button
           type="button"
           className="counter__button"
           aria-label="increment count"
+          onClick={handleIncreaseCounter}
         >
           +
         </button>
@@ -17,6 +29,7 @@ export default function Counter() {
           type="button"
           className="counter__button"
           aria-label="decrement count"
+          onClick={handleDecreaseCounter}
         >
           -
         </button>
