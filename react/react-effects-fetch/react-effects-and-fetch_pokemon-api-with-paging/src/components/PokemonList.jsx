9f5d6481cd72ba90cc346b42149453caf51042af
Simon Nancekievill <simon.nancekievill@gmail.com>
2026-04-17 11:35:07 +0200

    solving the pokemon paging challenge

--- react/react-effects-fetch/react-effects-and-fetch_pokemon-api-with-paging/src/components/PokemonList.jsx
+++ react/react-effects-fetch/react-effects-and-fetch_pokemon-api-with-paging/src/components/PokemonList.jsx
@@ -2,12 +2,13 @@ import { useEffect, useState } from "react";
 
 export default function PokemonList() {
   const [pokemon, setPokemon] = useState([]);
+  const [id, setId] = useState(0);
 
   useEffect(() => {
     async function loadPokemon() {
       try {
         const response = await fetch(
-          "https://pokeapi.co/api/v2/pokemon?offset=0"
+          `https://pokeapi.co/api/v2/pokemon?offset=${id}`,
         );
         const data = await response.json();
         setPokemon(data.results);
@@ -17,12 +18,27 @@ export default function PokemonList() {
     }
 
     loadPokemon();
-  }, []);
+  }, [id]);
+
+  if (id < 0) {
+    return (
+      <main>
+        <h1>No pokemons on the site…</h1>
+        <button type="button" onClick={() => setId(id + 20)}>
+          Add 20 pokemons to your list…
+        </button>
+      </main>
+    );
+  }
 
   return (
     <main>
-      <button type="button">Previous Page</button>
-      <button type="button">Next Page</button>
+      <button type="button" onClick={() => setId(id - 20)}>
+        Previous Page
+      </button>
+      <button type="button" onClick={() => setId(id + 20)}>
+        Next Page
+      </button>
       <ul>
         {pokemon.map(({ name }) => (
           <li key={name}>{name}</li>
