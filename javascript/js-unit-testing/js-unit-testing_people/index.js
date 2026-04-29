2be5bdd7ef21c8ed7c86675f0b7f9a4e26acef82
Simon Nancekievill <simon.nancekievill@gmail.com>
2026-04-29 16:57:59 +0200

    almost finishing people challenge

--- javascript/js-unit-testing/js-unit-testing_people/index.js
+++ javascript/js-unit-testing/js-unit-testing_people/index.js
@@ -2,17 +2,56 @@ export function getFirstNames(people) {
   return people.map((person) => person.firstName);
 }
 
-export function getFullNames(people) {}
+export function getFullNames(people) {
+  return people.map((person) => `${person.firstName} ${person.lastName}`);
+}
 
-export function getNameAndAge(people) {}
+export function getNameAndAge(people) {
+  return people.map((person) => `${person.lastName} (${person.age})`);
+}
 
-export function getPeopleByAge(people, age) {}
+export function getPeopleByAge(people, age) {
+  /* Durch peolpe array durchlaufen um zu schauen ob das alter mit dem angegebenen Alter übereinstimmt, wenn ja, 
+aus den personen ein Array erstellen mit den Nachnamen.
+*/
 
-export function getPeopleNamesOlderThan(people, age) {}
+  /* const filteredAge = people.filter((person) => person.age === age) 
+ return filteredAge.map((person) => `${person.lastName}`)
+*/
 
-export function getPeopleByLastName(people, lastName) {}
+  return people
+    .filter((person) => person.age === age)
+    .map((person) => `${person.lastName}`);
+}
 
-export function findPersonById(people, id) {}
+export function getPeopleNamesOlderThan(people, age) {
+  /*über das people array laufen, und schauen ob das alter größer ist als das angegebenen Alter, wenn ja, aus den ausgewählten ein array erstellen
+mit dem Namen als String.*/
+  return people
+    .filter((person) => person.age > age)
+    .map((person) => `${person.firstName} ${person.lastName}`);
+
+  // const filteredAge = people.filter((person) => person.age < age);
+  // return filteredAge.toSorted(
+  //   filteredAge.map((person) => `${person.firstName} ${person.lastName}`)
+  // );
+}
+
+export function getPeopleByLastName(people, lastName) {
+  /*über das people array laufen und den eingegebenen Namen mit den namen vergleichen, alle übereinstimmungen in ein Array packen und den vollen namen ausgeben.*/
+
+  return people
+    .filter((person) => person.lastName === lastName)
+    .map((person) => `${person.firstName} ${person.lastName}`);
+}
+
+export function findPersonById(people, id) {
+  /*über das people array laufen und sobald die id stimmt, die person als ganzes object ausgeben*/
+
+  return people
+    .filter((person) => person.id === id)
+    .map((person) => Object.fromEntries(`id: {person.id}`));
+}
 
 export function isAnyoneOlderThan(people, age) {}
 
