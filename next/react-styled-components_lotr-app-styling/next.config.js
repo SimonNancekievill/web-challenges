11933d0a6fed90439133a0ef81b6c70f9d16dd55
Simon Nancekievill <simon.nancekievill@gmail.com>
2026-05-06 16:03:23 +0200

    I dont get it

--- next/react-styled-components_lotr-app-styling/next.config.js
+++ next/react-styled-components_lotr-app-styling/next.config.js
@@ -1,5 +1,18 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
+  // {...}
+  images: {
+    domains: ["upload.wikimedia.org"],
+  },
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ["@svgr/webpack"],
+    });
+    return config;
+  },
   compiler: {
     styledComponents: true,
   },
--- next/react-styled-components_lotr-app-styling/pages/volumes/[slug].js
+++ next/react-styled-components_lotr-app-styling/pages/volumes/[slug].js
@@ -2,6 +2,19 @@ import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import { volumes } from "../../lib/data";
+import styled from "styled-components";
+
+// const main = styled.div`
+//   background-color: ${(probs) =>
+//     probs.$color === "color" ? "color" : "white"};
+// `;
+
+const StyledLink = styled(Link)`
+  font-family: var(--body);
+`;
+const StyledHeadline1 = styled.h1`
+  font-family: var(--font-headline-1);
+`;
 
 export default function VolumeDetail() {
   const router = useRouter();
@@ -17,40 +30,42 @@ export default function VolumeDetail() {
     return null;
   }
 
-  const { title, description, cover, books } = volume;
+  const { title, description, cover, books, color } = volume;
 
   return (
     <>
-      <Link href="/volumes">← All Volumes</Link>
-      <h1>{title}</h1>
-      <p>{description}</p>
-      <ul>
-        {books.map(({ ordinal, title }) => (
-          <li key={title}>
-            {ordinal}: <strong>{title}</strong>
-          </li>
-        ))}
-      </ul>
-      <Image
-        src={cover}
-        alt={`Cover image of ${title}`}
-        width={140}
-        height={230}
-      />
-      {previousVolume ? (
-        <div>
-          <Link href={`/volumes/${previousVolume.slug}`}>
-            ← Previous Volume: {previousVolume.title}
-          </Link>
-        </div>
-      ) : null}
-      {nextVolume ? (
-        <div>
-          <Link href={`/volumes/${nextVolume.slug}`}>
-            Next Volume: {nextVolume.title} →
-          </Link>
-        </div>
-      ) : null}
+      <main>
+        <StyledLink href="/volumes">← All Volumes</StyledLink>
+        <StyledHeadline1>{title}</StyledHeadline1>
+        <p>{description}</p>
+        <ul>
+          {books.map(({ ordinal, title, slug }) => (
+            <li key={slug}>
+              {ordinal}: <strong>{title}</strong>
+            </li>
+          ))}
+        </ul>
+        <Image
+          src={cover}
+          alt={`Cover image of ${title}`}
+          width={140}
+          height={230}
+        />
+        {previousVolume ? (
+          <div>
+            <Link href={`/volumes/${previousVolume.slug}`}>
+              ← Previous Volume: {previousVolume.title}
+            </Link>
+          </div>
+        ) : null}
+        {nextVolume ? (
+          <div>
+            <Link href={`/volumes/${nextVolume.slug}`}>
+              Next Volume: {nextVolume.title} →
+            </Link>
+          </div>
+        ) : null}
+      </main>
     </>
   );
 }
