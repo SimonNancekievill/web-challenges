780a36a23f648d1f4dd9e07afbb27954587b1ed7
Simon Nancekievill <simon.nancekievill@gmail.com>
2026-05-11 15:06:40 +0200

    solving greeting challenge

--- backend/basics/backend-basics_greeting/index.js
+++ backend/basics/backend-basics_greeting/index.js
@@ -1,3 +1,8 @@
 // import { server } from "./server.js";
-
+import { server } from "./server.js";
 // server.listen(…
+
+const port = 8000;
+server.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
