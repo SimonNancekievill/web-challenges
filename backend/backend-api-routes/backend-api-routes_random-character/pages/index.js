b9b3d0517b72962b9918ab8f6a95e3ab7171ee51
Simon Nancekievill <simon.nancekievill@gmail.com>
2026-05-12 12:08:50 +0200

    solving random characters challenge

--- backend/backend-api-routes/backend-api-routes_random-character/pages/index.js
+++ backend/backend-api-routes/backend-api-routes_random-character/pages/index.js
@@ -1,3 +1,22 @@
+import useSWR from "swr";
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
 export default function HomePage() {
-  return <h1>Hello from Next.js!</h1>;
+  const { data, isLoading } = useSWR("/api/random-character", fetcher);
+
+  if (!data) return <div>failed to load</div>;
+  if (isLoading) return <div>loading...</div>;
+  console.log(data);
+
+  const { character } = data;
+
+  return (
+    <>
+      <h1>Random Character</h1>
+      <h2>
+        {character.firstName} {character.lastName}
+      </h2>
+      <p>Twitter Name: {character.twitterName}</p>
+      <p>This ist my Geohash: {character.geohash}</p>
+    </>
+  );
 }
