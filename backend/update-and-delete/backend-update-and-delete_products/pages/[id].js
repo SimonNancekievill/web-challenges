8cce47c4b8922e378550c30efd4f907d5db82a5c
Simon Nancekievill <simon.nancekievill@gmail.com>
2026-05-19 12:25:12 +0200

    hilfe feline

--- backend/update-and-delete/backend-update-and-delete_products/pages/[id].js
+++ backend/update-and-delete/backend-update-and-delete_products/pages/[id].js
@@ -2,13 +2,32 @@ import useSWR from "swr";
 import styled from "styled-components";
 import { useRouter } from "next/router";
 import StyledLink from "@/components/Link";
+import { useState } from "react";
+import ProductForm from "@/components/ProductForm";
 
 export default function Product() {
+  const [isEditMode, setIsEditMode] = useState(false);
   const router = useRouter();
   const { id } = router.query;
 
-  const { data, isLoading } = useSWR(`/api/products/${id}`);
+  const { data, isLoading, mutate } = useSWR(`/api/products/${id}`);
 
+  async function handleEditProduct(event) {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    const productData = Object.fromEntries(formData);
+    console.log(productData);
+
+    const response = await fetch(`/api/products/${id}`, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(productData),
+    });
+    if (response.ok) {
+      mutate();
+    }
+  }
   if (isLoading) {
     return <h1>Loading...</h1>;
   }
@@ -24,6 +43,26 @@ export default function Product() {
       <p>
         Price: {data.price} {data.currency}
       </p>
+      <div>
+        <button
+          type="button"
+          onClick={() => {
+            setIsEditMode(!isEditMode);
+          }}
+        >
+          <span role="img" aria-label="A pencil">
+            ✏️
+          </span>
+        </button>
+        <button>
+          <span role="img" aria-label="A cross indicating deletion">
+            ❌
+          </span>
+        </button>
+      </div>
+      {isEditMode && (
+        <ProductForm onSubmit={handleEditProduct} value={data} isEditMode />
+      )}
       <StyledLink href="/">Back to all</StyledLink>
     </ProductCard>
   );
